491e7c1830819b0bf53482c34d51922ca5d185f1
kelvintandrio <herwandatandrio@gmail.com>
2019-09-18 11:42:54 +0700

    Model UI TabItem

--- app/src/main/java/algorithm/kelvin/project/ca_info/view/MainActivity.kt
+++ app/src/main/java/algorithm/kelvin/project/ca_info/view/MainActivity.kt
@@ -2,9 +2,11 @@ package algorithm.kelvin.project.ca_info.view
 
 import algorithm.kelvin.project.ca_info.R
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private val movie = MovieFragment()
     private val tv = TVFragment()
@@ -13,8 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var tabNow = tabMovie
+        tabNow.setBackgroundColor(resources.getColor(R.color.colorTabItem))
+
         val mainPageAdapter = MainPageAdapter(supportFragmentManager)
         mainPageAdapter.addPageMenu(movie, tv)
         main_viewpager.adapter = mainPageAdapter
+
+        imageMovie.setOnClickListener {
+            main_viewpager.currentItem = 0
+            tabNow.setBackgroundColor(resources.getColor(R.color.colorWhite))
+            tabNow = tabMovie
+            tabNow.setBackgroundColor(resources.getColor(R.color.colorTabItem))
+        }
+
+        imageTV.setOnClickListener {
+            main_viewpager.currentItem = 1
+            tabNow.setBackgroundColor(resources.getColor(R.color.colorWhite))
+            tabNow = tabTV
+            tabNow.setBackgroundColor(resources.getColor(R.color.colorTabItem))
+        }
     }
 }
