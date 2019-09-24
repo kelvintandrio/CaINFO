1d3ad35041fc83e01e5af62887ce844f3e9b16de
kelvintandrio <herwandatandrio@gmail.com>
2019-09-24 11:20:14 +0700

    Create modularization for tvApps

---
+++ tvApps/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(Dependencies.Android.compileSdkVersion)
+
+    defaultConfig {
+        buildConfigField("String", "URL_MOVIE_CATALOG", "\"https://api.themoviedb.org\"")
+        buildConfigField("String", "API_KEY", "\"5a9914ea9eb3bfc1addfe13b0b92b0b4\"")
+        buildConfigField("String", "URL_IMAGE", "\"https://image.tmdb.org/t/p/w185\"")
+        buildConfigField("String", "URL_POSTER", "\"https://image.tmdb.org/t/p/w780\"")
+
+        minSdkVersion(Dependencies.Android.minSdkVersion)
+        targetSdkVersion(Dependencies.Android.targetSdkVersion)
+        versionCode = Dependencies.Android.versionCode
+        versionName = Dependencies.Android.versionName
+        testInstrumentationRunner = Dependencies.Android.testInstrumentationRunner
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(Dependencies.Kotlin.kotlin_std)
+
+    implementation(Dependencies.SupportLibrary.legacySupport)
+    implementation(Dependencies.SupportLibrary.lifecycleLibrary)
+    kapt(Dependencies.SupportLibrary.lifecycleCompiler)
+    implementation(Dependencies.SupportLibrary.recyclerView)
+    implementation(Dependencies.SupportLibrary.materialSupport)
+    implementation(Dependencies.SupportLibrary.pagingLibrary)
+    implementation(Dependencies.SupportLibrary.pagingRxJavaLibrary)
+    implementation(Dependencies.SupportLibrary.glideLibrary)
+
+    implementation(project(":itemData"))
+    implementation(project(":repository"))
+}
---
+++ tvApps/src/main/java/algorithm/kelvin/tvApps/AiringToday.kt
@@ -0,0 +1,16 @@
+package algorithm.kelvin.tvApps
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AiringToday : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_airing_today, container, false)
+    }
+
+
+}
