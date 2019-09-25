8aaca995c05bbe8744b1ca2a60cea36a47572b52
Kelvin Herwanda T <herwandatandrio@gmail.com>
2019-09-25 16:50:21 +0700

    Undo merge

--- movieApps/src/main/java/algorithm/kelvin/project/movieApps/Related.kt
+++ movieApps/src/main/java/algorithm/kelvin/project/movieApps/Related.kt
@@ -9,7 +9,6 @@ import android.view.ViewGroup
 class Related : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_related, container, false)
     }
 }
