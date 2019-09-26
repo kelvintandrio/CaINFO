ae74506b50750aa70c70857a6ab61b654bf44439
Kelvin Herwanda T <herwandatandrio@gmail.com>
2019-09-26 14:31:30 +0700

    settings build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,4 @@ dependencies {
     testImplementation(Dependencies.TestingLibrary.testJunit)
     androidTestImplementation(Dependencies.TestingLibrary.androidTestRunner)
     androidTestImplementation(Dependencies.TestingLibrary.androidTestEspresso)
-
-    implementation(project(":movieApps"))
 }
